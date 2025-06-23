0d031dc3f95af79a6385ac86d789e58511bff318
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-06-23 15:30:21 +0100

    Random X Line Animation

--- Program.cs
+++ Program.cs
@@ -1,44 +1,50 @@
 ﻿//Spinning Donut!
 using System;
+using System.Threading;
 class Program
 {
     static void Main()
     {
-        int width = 20;
-        int height =20;
+        int width = 10;
+        int height =10;
         char[,] canvas = new char[height, width];
-
-        //Clear Canvas Before Frame
-        for (int y = 0; y < height; y++)
+        Console.CursorVisible = false;
+        Random rnd = new Random();
+        while (true)
         {
-            for (int x = 0; x < width; x++)
-            {
-                canvas[y, x] = ' ';
-            }
-        }
+            int rndInt = rnd.Next(1, 7);
 
-        //Line Render
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
             {
-                if(x == y)
+                for (int x = 0; x < width; x++)
                 {
-                    canvas[y, x] = '.';
+                    canvas[y, x] = ' ';
                 }
             }
-        }
-
-
-
-        //Canvas render
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
+            
+            //Line Render
+            for (int y = 0; y < height; y++)
             {
-                Console.Write(canvas[y, x]);
+                for (int x = 0; x < width; x++)
+                {
+                    if (x ==rndInt)
+                    {
+                        canvas[y, x] = '.';
+                    }
+                }
             }
-            Console.WriteLine();
+
+            //Canvas render
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(canvas[y, x]);
+                }
+                Console.WriteLine();
+            }
+            Console.SetCursorPosition(0, 0);
+            Thread.Sleep(50);
         }
     }
 }
