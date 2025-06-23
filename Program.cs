75c453da6e558851f38f57f34aa393e5e954ad19
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-06-23 14:33:34 +0100

    Grid System Working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
 ﻿//Spinning Donut!
-Console.WriteLine("Hello World");
+using System;
+class Program
+{
+    static void Main()
+    {
+        int width = 20;
+        int height =20;
+        char[,] canvas = new char[height, width];
+
+        //Clear Canvas Before Frame
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                canvas[y, x] = ' ';
+            }
+        }
+
+        //Line Render
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if(x == y)
+                {
+                    canvas[y, x] = '.';
+                }
+            }
+        }
+
+
+
+        //Canvas render
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Console.Write(canvas[y, x]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
