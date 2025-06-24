1ac951c4249dd1702c7ea22d9ffd0c7ab215e0f7
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-06-24 14:13:51 +0100

    StringBuilding and Buffersize To fix Scrolling Issue

--- Program.cs
+++ Program.cs
@@ -1,62 +1,60 @@
 ﻿using System;
+using System.Text;
 using System.Threading;
 
-class Program
+int width = 90;
+int height = 30;
+Console.SetWindowSize(width, height + 1);
+Console.SetBufferSize(width, height + 1);
+double K1 = 2.0;
+double r1 = 10;
+double r2 = 5;
+
+double thetaSpacing = 0.07;
+double phiSpacing = 0.02;
+
+char[,] canvas = new char[height, width];
+Console.CursorVisible = false;
+
+while (true)
 {
-    static void Main()
+    // Clear canvas
+    for (int y = 0; y < height; y++)
+        for (int x = 0; x < width; x++)
+            canvas[y, x] = ' ';
+
+    for (double theta = 0; theta < 2 * Math.PI; theta += thetaSpacing)
     {
-        int width = 80;
-        int height = 40;
-
-
-        double K1 = 2.0;
-        double r1 = 10;
-        double r2 = 5;
-
-        double thetaSpacing = 0.07;
-        double phiSpacing = 0.02;
-
-        char[,] canvas = new char[height, width];
-        Console.CursorVisible = false;
-
-        while (true)
+        for (double phi = 0; phi < 2 * Math.PI; phi += phiSpacing)
         {
-            Console.Clear();
-            // Clear canvas
-            for (int y = 0; y < height; y++)
-                for (int x = 0; x < width; x++)
-                    canvas[y, x] = ' ';
+            double x = (r1 + r2 * Math.Cos(theta)) * Math.Cos(phi);
+            double y = (r1 + r2 * Math.Cos(theta)) * Math.Sin(phi);
+            double z = r2 * Math.Sin(theta);
 
-            for(double theta = 0; theta < 2 * Math.PI; theta+= thetaSpacing)
+            int x_proj = (int)((width / 2) + K1 * x);
+            int y_proj = (int)((height / 2) - K1 * y);
+
+            if (x_proj >= 0 && x_proj < width && y_proj >= 0 && y_proj < height)
             {
-                for(double phi = 0; phi < 2 * Math.PI; phi += phiSpacing)
-                {
-                    double x = (r1 + r2 * Math.Cos(theta)) * Math.Cos(phi);
-                    double y = (r1 + r2 * Math.Cos(theta)) * Math.Sin(phi);
-                    double z = r2 * Math.Sin(theta);
-
-                    int x_proj = (int)((width / 2) + K1 * x);
-                    int y_proj = (int)((height / 2) - K1 * y);
-
-                    if (x_proj >= 0 && x_proj < width && y_proj >= 0 && y_proj < height)
-                    {
-                        canvas[y_proj, x_proj] = '.';
-                    }
-
-                }
-            }
-            
-
-            // Render to console
-            Console.SetCursorPosition(0, 0);
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                    Console.Write(canvas[y, x]);
-                Console.WriteLine();
+                canvas[y_proj, x_proj] = '.';
             }
 
-            Thread.Sleep(100);
         }
     }
+    Console.SetCursorPosition(0, 0);
+
+    // Render to console
+    StringBuilder sb = new StringBuilder();
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+            sb.Append(canvas[y, x]);
+
+        if (y < height - 1)
+            sb.Append('\n'); // Don't add \n after the last line
+    }
+    Console.Write(sb.ToString());
+
+    Thread.Sleep(100);
+
 }
