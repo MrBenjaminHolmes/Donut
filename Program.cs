059ccd5bfb30078db0091260381be9c2dfc1b3fc
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-06-24 18:04:33 +0100

    Multiplied Rotations

--- Program.cs
+++ Program.cs
@@ -11,12 +11,19 @@ double r2 = 4;
 double thetaSpacing = 0.07;
 double phiSpacing = 0.02;
 double A = 0;
+double B = 0;
 
 char[,] canvas = new char[height, width];
 Console.CursorVisible = false;
 
 while (true)
 {
+
+    double cosA = Math.Cos(A);
+    double SinA = Math.Sin(A);
+    double cosB = Math.Cos(B);
+    double SinB = Math.Sin(B);
+
     // Clear canvas
     for (int y = 0; y < height; y++)
         for (int x = 0; x < width; x++)
@@ -34,9 +41,13 @@ while (true)
             //y = (y*Math.Cos(A)) - (z*Math.Sin(A));
             //z = (y * Math.Sin(A)) + (z * Math.Cos(A));
             //Y Rotation
+            //x=(x*Math.Cos(A))+(z*Math.Sin(B));
+            //z=(-x*Math.Sin(A))+(z*Math.Cos(B));
 
-            x=(x*Math.Cos(A))+(z*Math.Sin(A));
-            z=(-x*Math.Sin(A))+(z*Math.Cos(A));
+            x = x * cosB + (y * SinA + z * cosA) * SinB;
+            y = y * cosA - (z * SinA);
+            z = -x*SinB + (y*SinA + z*cosA) * cosB;
+
 
             int x_proj = (int)((width / 2) + K1 * x);
             int y_proj = (int)((height / 2) - K1 * y * 0.5);
@@ -67,5 +78,10 @@ while (true)
     if (A >= Math.Tau)
     {
         A = 0;
+    }
+    B += 0.1;
+    if (B >= Math.Tau)
+    {
+        B = 0;
     }
 }
